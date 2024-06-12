296f105bb541132b0989a01ff866de84e2dc21fb
apwic <adiyansawicaksana@gmail.com>
2024-06-12 17:48:46 +0700

    feat: using sioyek for pdf-viewer

--- lua/kickstart/plugins/vimtex.lua
+++ lua/kickstart/plugins/vimtex.lua
@@ -4,9 +4,17 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_view_method = 'sioyek'
     vim.g.vimtex_compiler_latexmk = {
       out_dir = 'output',
     }
+  end,
+  config = function()
+    vim.cmd [[
+      augroup vimtex
+        autocmd!
+        autocmd User VimtexEventViewReverse call b:vimtex.viewer.xdo_focus_vim()
+      augroup END
+    ]]
   end,
 }
