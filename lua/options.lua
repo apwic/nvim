7dabf9f9d6ffbb65316a2dec1ad5e881318e3353
apwic <adiyansawicaksana@gmail.com>
2024-11-08 18:10:14 +0700

    fix: remove list and listchar usage

--- lua/options.lua
+++ lua/options.lua
@@ -47,8 +47,8 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
@@ -62,7 +62,7 @@ vim.opt.scrolloff = 50
 -- Tab size
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 
 -- Line numbers etc in Netrw
