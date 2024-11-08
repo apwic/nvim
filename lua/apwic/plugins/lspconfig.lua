8163dd2a2e3f0633c9ecd3e1985bcb6c9f2ba338
apwic <adiyansawicaksana@gmail.com>
2024-11-08 16:52:48 +0700

    feat: gopls server

--- lua/apwic/plugins/lspconfig.lua
+++ lua/apwic/plugins/lspconfig.lua
@@ -162,6 +162,7 @@ return {
         -- rust_analyzer = {},
         markdown_oxide = {},
         texlab = {},
+        gopls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
