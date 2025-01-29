7685c6b9442f643a1a42086dbe9ca6de88ab7c06
apwic <adiyansawicaksana@gmail.com>
2025-01-29 19:10:00 +0700

    go plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -74,6 +74,8 @@ require('lazy').setup({
 
   require 'apwic.plugins.harpoon',
 
+  require 'apwic.plugins.go',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
