d812ea2be0deaddbb960870f5253164b2b68bd5e
apwic <adiyansawicaksana@gmail.com>
2025-02-19 10:28:28 +0700

    diagnostics by severity telescope

--- lua/apwic/plugins/telescope.lua
+++ lua/apwic/plugins/telescope.lua
@@ -95,7 +95,16 @@ return {
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+      -- Diagnostics Telescope
+      vim.keymap.set('n', '<leader>sdd', builtin.diagnostics, { desc = '[S]earch by [D]iagnostics [D] All' })
+      vim.keymap.set('n', '<leader>sdq', function()
+        builtin.diagnostics { severity = vim.diagnostic.severity.ERROR }
+      end, { desc = '[S]earch [D]iagnostics [E]rror' })
+      vim.keymap.set('n', '<leader>sdw', function()
+        builtin.diagnostics { severity = vim.diagnostic.severity.WARN }
+      end, { desc = '[S]earch [D]iagnostics [W]arn' })
+
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
