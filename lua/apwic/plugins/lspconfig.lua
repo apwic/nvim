3843bc6892145415d50ebfe056368105da2400c7
apwic <adiyansawicaksana@gmail.com>
2025-05-21 16:02:12 +0700

    fix fe lsp

--- lua/apwic/plugins/lspconfig.lua
+++ lua/apwic/plugins/lspconfig.lua
@@ -179,7 +179,7 @@ return {
             },
           },
         },
-        tsserver = {},
+        ts_ls = {},
         html = {},
         cssls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
