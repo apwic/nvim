4f105c8f192ec5fa8f62087e3ac9499e74139300
apwic <adiyansawicaksana@gmail.com>
2026-03-30 22:49:23 +0700

    defer treesitter checker for faster startup time

--- lua/apwic/plugins/treesitter.lua
+++ lua/apwic/plugins/treesitter.lua
@@ -2,49 +2,49 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    lazy = false,
     config = function()
       require('nvim-treesitter').setup()
-
-      -- Prefer git instead of curl in order to improve connectivity in some environments
       require('nvim-treesitter.install').prefer_git = true
 
-      -- Ensure parsers are installed (new API replaces old ensure_installed)
-      local ensure_installed = {
-        'bash',
-        'c',
-        'diff',
-        'html',
-        'css',
-        'javascript',
-        'typescript',
-        'lua',
-        'luadoc',
-        'markdown',
-        'vim',
-        'vimdoc',
-        'json',
-        'go',
-      }
-
-      local installed = require('nvim-treesitter.config').get_installed()
-      local to_install = vim.tbl_filter(function(lang)
-        return not vim.list_contains(installed, lang)
-      end, ensure_installed)
-
-      if #to_install > 0 then
-        require('nvim-treesitter').install(to_install)
-      end
-
-      -- Enable treesitter highlighting and auto-install missing parsers
+      -- Enable treesitter highlighting per buffer
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
-          local ft = vim.bo[args.buf].filetype
-          local lang = vim.treesitter.language.get_lang(ft)
+          local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
           if lang and pcall(vim.treesitter.language.inspect, lang) then
             vim.treesitter.start(args.buf, lang)
           end
         end,
       })
+
+      -- Defer parser installation check so it doesn't block startup
+      vim.defer_fn(function()
+        local ensure_installed = {
+          'bash',
+          'c',
+          'diff',
+          'html',
+          'css',
+          'javascript',
+          'typescript',
+          'lua',
+          'luadoc',
+          'markdown',
+          'vim',
+          'vimdoc',
+          'json',
+          'go',
+        }
+
+        local installed = require('nvim-treesitter.config').get_installed()
+        local to_install = vim.tbl_filter(function(lang)
+          return not vim.list_contains(installed, lang)
+        end, ensure_installed)
+
+        if #to_install > 0 then
+          require('nvim-treesitter').install(to_install)
+        end
+      end, 0)
     end,
   },
 }
