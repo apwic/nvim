22d90e24cb6db53fb0629c85febd49d7c522adb3
apwic <adiyansawicaksana@gmail.com>
2026-06-15 18:09:41 +0700

    markdown preview to use the cmux browser

--- lua/apwic/plugins/markdown-preview.lua
+++ lua/apwic/plugins/markdown-preview.lua
@@ -4,12 +4,13 @@ return {
   ft = { 'markdown' },
   build = 'cd app && yarn install',
   config = function()
+    local scripts_dir = vim.fn.stdpath 'config' .. '/scripts'
     vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
-    vim.cmd [[
+    vim.cmd(string.format([[
       function! OpenMarkdownPreview(url)
-        silent execute '!cmux browser open-split ' . shellescape(a:url)
+        silent execute '!%s/cmux-browser-open ' . shellescape(a:url)
       endfunction
-    ]]
+    ]], scripts_dir))
 
     vim.keymap.set('n', '<leader>po', '<cmd>MarkdownPreview<CR>', { desc = 'Markdown Preview Open' })
     vim.keymap.set('n', '<leader>pc', '<cmd>MarkdownPreviewStop<CR>', { desc = 'Markdown Preview Close' })
